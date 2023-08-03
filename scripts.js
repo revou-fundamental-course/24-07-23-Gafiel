6d99bebd6fd29bc7d872488db14fc3a59464602f
Ghafirul Pratama <140412685+Gafiel@users.noreply.github.com>
2023-08-03 11:50:40 +0700

    Delete scripts.js

--- scripts.js
+++
@@ -1,5 +0,0 @@
-const selectElement = document.getElementById("selectOption");
-selectElement.addEventListener("change", () => {
-  const selectedOption = selectElement.options[selectElement.selectedIndex];
-  console.log("Pilihan:", selectedOption.value, selectedOption.text);
-});
